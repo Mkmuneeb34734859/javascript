bb0fbc0efc0b9f8174ebe8252e62002dfdda44db
Mkmuneeb34734859 <154329450+Mkmuneeb34734859@users.noreply.github.com>
2024-05-19 17:12:20 +0500

    texteditor

---
+++ index.js
@@ -0,0 +1,11 @@
+var maintext = document.getElementById('heading')
+
+function bold () {
+    maintext.style.fontWeight = "1000"
+}
+function italic () {
+    maintext.style.fontStyle = "Italic"
+}
+function underLine () {
+    maintext.style.textDecoration = "underLine"
+}
